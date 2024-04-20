6047b36170ef9183b248574880278ed02af909ed
Memorywx <2829327570@qq.com>
2024-04-20 23:33:44 +0800

    first push

--- proj1c/src/deque/MaxArrayDeque61B.java
+++ proj1c/src/deque/MaxArrayDeque61B.java
@@ -1,2 +1,18 @@
-package deque;public class MaxArrayDeque61B {
+package deque;
+
+import java.util.Comparator;
+
+public class MaxArrayDeque61B<T> extends ArrayDeque61B<T> {
+
+    public MaxArrayDeque61B(Comparator<T> c) {
+        MaxArrayDeque61B<T> m = new MaxArrayDeque61B<>(c);
+    }
+
+    public T max() {
+        if ( this == null) {
+            return null;
+        }
+    }
+
+
 }
