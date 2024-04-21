acc9ac2017642f8df6d6d175b4d8b0c31f1f876d
Memorywx <2829327570@qq.com>
2024-04-21 23:50:16 +0800

    completed the MaxArrayDeque61B

--- proj1c/src/deque/MaxArrayDeque61B.java
+++ proj1c/src/deque/MaxArrayDeque61B.java
@@ -4,15 +4,63 @@ import java.util.Comparator;
 
 public class MaxArrayDeque61B<T> extends ArrayDeque61B<T> {
 
-    public MaxArrayDeque61B(Comparator<T> c) {
-        MaxArrayDeque61B<T> m = new MaxArrayDeque61B<>(c);
-    }
+    private Comparator<T> my_c;
 
-    public T max() {
-        if ( this == null) {
-            return null;
+    public static class stringComparator implements Comparator<String> {
+        @Override
+        public int compare(String o1, String o2) {
+            return o1.compareTo(o2);
         }
     }
 
+    public MaxArrayDeque61B(Comparator<T> c) {
+        my_c = c;
+    }
 
+
+    public T max() {
+        if ( this.isEmpty() ) {
+            return null;
+        }
+
+        T maxItem = this.get(0);
+        for ( T x : this ) {
+            if ( my_c.compare(x, maxItem ) > 0 ) {
+                maxItem = x;
+            }
+        }
+        return maxItem;
+    }
+
+    public T max(Comparator<T> c) {
+        if ( this.isEmpty() ) {
+            return null;
+        }
+
+        T maxItem = this.get(0);
+        for ( T x : this ) {
+            if ( c.compare(x, maxItem) > 0 ) {
+                maxItem = x;
+            }
+        }
+        return maxItem;
+    }
+
+
+    public static void main(String[] args) {
+        MaxArrayDeque61B<Integer> m = new MaxArrayDeque61B<Integer>(Comparator.naturalOrder());
+        m.addFirst(999);
+        m.addFirst(65926);
+        m.addLast(78);
+        m.addLast(8898797);
+        System.out.println(m.max(Comparator.naturalOrder()));
+
+        MaxArrayDeque61B<String> l = new MaxArrayDeque61B<>(new stringComparator());
+        l.addLast("asdjhiq");
+        l.addFirst("pqwje");
+        l.addLast("zoa");
+        l.addLast("zzz");
+        System.out.println(l.max());
+
+    }
 }
