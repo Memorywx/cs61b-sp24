c74618a9af2821b270912cadbdb5d53a5f1400a1
Memorywx <2829327570@qq.com>
2024-07-13 16:52:37 +0800

    updated

--- proj1a/src/LinkedListDeque61B.java
+++ proj1a/src/LinkedListDeque61B.java
@@ -3,68 +3,58 @@ import java.util.List;
 
 public class LinkedListDeque61B<T> implements Deque61B<T> {
 
-    public class Node{
-        T item;
-        Node prev;
-        Node next;
+    private class Node {
+        private T item;
+        private Node prev;
+        private Node next;
 
-        public Node(Node f, T i, Node n){
-            item = i;
-            prev = f;
-            next = n;
+        private Node(T item, Node prev, Node next) {
+            this.item = item;
+            this.prev = prev;
+            this.next = next;
         }
-
     }
 
-    public Node sentinel;
-    public int size;
+    private Node sentinel;
+    private int size;
 
 
-    public LinkedListDeque61B(){
+    public LinkedListDeque61B() {
         sentinel = new Node(null, null, null);
-        // 不能直接 sentinel = new Node(sentinel, null, sentinel);
-        // 因为此时 sentinel还没保存地址，为null。
         sentinel.prev = sentinel;
         sentinel.next = sentinel;
         size = 0;
-
     }
-
     @Override
     public void addFirst(T x) {
-        Node firstNode = new Node(sentinel, x, sentinel.next);
-        sentinel.next.prev = firstNode;
-        sentinel.next = firstNode;
+        Node node = new Node(x, sentinel, sentinel.next);
+        sentinel.next.prev = node;
+        sentinel.next = node;
         size++;
     }
 
     @Override
     public void addLast(T x) {
-        Node lastNode = new Node(sentinel.prev, x, sentinel);
-        sentinel.prev.next = lastNode;
-        sentinel.prev = lastNode;
+        Node node = new Node(x, sentinel.prev, sentinel);
+        sentinel.prev.next = node;
+        sentinel.prev = node;
         size++;
-
     }
 
     @Override
     public List<T> toList() {
-        List<T> returnList = new ArrayList<>();
-        Node p = sentinel;
-        while (p.next != sentinel){
+        ArrayList l = new ArrayList<>();
+        Node p = sentinel.next;
+        while (p != sentinel) {
+            l.add(p.item);
             p = p.next;
-            returnList.add(p.item);
         }
-
-        return returnList;
+       return l;
     }
 
     @Override
     public boolean isEmpty() {
-        if (sentinel.next == sentinel){
-            return true;
-        }
-        return false;
+        return size == 0;
     }
 
     @Override
@@ -74,76 +64,48 @@ public class LinkedListDeque61B<T> implements Deque61B<T> {
 
     @Override
     public T removeFirst() {
-        if (isEmpty()) {
-            return null;
-        }
-        T removedFirstItem = sentinel.next.item;
+        Node removedNode = sentinel.next;
         sentinel.next = sentinel.next.next;
-        sentinel.next.next.prev = sentinel;
+        sentinel.next.prev = sentinel;
         size--;
-        return removedFirstItem;  // 返回被删除掉的LinkedList元素
+        return removedNode.item;
     }
 
     @Override
     public T removeLast() {
-        if (isEmpty()) {
-            return null;
-        }
-
-        T removedLastItem = sentinel.prev.item;
-        sentinel.prev.prev.next = sentinel;
+        Node removedNode = sentinel.prev;
         sentinel.prev = sentinel.prev.prev;
+        sentinel.prev.next = sentinel;
         size--;
-        return removedLastItem;
-
+        return removedNode.item;
     }
 
     @Override
     public T get(int index) {
-        if (index < 0 || index >= size)
+        if (index < 0 || index >= size) {
             return null;
-
+        }
         Node p = sentinel.next;
         int i = 0;
-        while (i != index){
+        while (i < index) {
             p = p.next;
             i++;
         }
         return p.item;
-
     }
 
     @Override
     public T getRecursive(int index) {
-        return getRecursiveHelper(sentinel, index);
-    }
-
-
-    // 因为Node类不具备getRecursive方法, 因此得有个帮手。
-    public T getRecursiveHelper(Node p, int index){
         if (index < 0 || index >= size) {
             return null;
         }
-
-        if (index == 0){
-            return p.next.item;     // return getRecursiveHelper(sentinel, index);
-        }                           // 由于是由sentinel开始的，所以需要写 p.next.item
-                                    // 若是从sentinel.next开始，直接写 p.item 就好了
-
-        return getRecursiveHelper(p.next, index - 1);
+        return getHelper(index, sentinel).item;
     }
 
-
-    public static void main(String[] args) {
-        LinkedListDeque61B<Integer> L = new LinkedListDeque61B<>();
-        L.addFirst(5);
-        L.addFirst(6);
-        LinkedListDeque61B<Integer> p = new LinkedListDeque61B<>();
-        p.addLast(9);
-        p.addLast(7);
-        p.addFirst(4);
-        Deque61B<String> someList = new LinkedListDeque61B<>();
-        someList.addFirst("ELK");
-        System.out.println(someList.get(0));
+    public Node getHelper(int index, Node p) {
+        if (index == 0) {
+            return p.next;
+        }
+        return getHelper(index - 1, p.next);
     }
 }
