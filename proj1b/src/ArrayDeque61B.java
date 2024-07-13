dc0508e085dd7a0d0cf713a9d9cd531b378e5f35
Memorywx <2829327570@qq.com>
2024-07-14 00:06:21 +0800

    updated

--- proj1b/src/ArrayDeque61B.java
+++ proj1b/src/ArrayDeque61B.java
@@ -9,42 +9,59 @@ public class ArrayDeque61B<T> implements Deque61B<T>{
     private int nextFirst;
     private int nextLast;
     private int size;
+    private final int initialLength = 8;
 
-    public ArrayDeque61B(){
-        items = (T[]) new Object[8];
-        size = 0;
+    private final double LessenFactor = 0.25;
+    private final double ExpandFactor = 0.75;
+
+    public ArrayDeque61B() {
+        items = (T[]) new Object[initialLength];
         nextFirst = 0;
         nextLast = 1;
+        size = 0;
     }
-
-    public void whetherResize(){
-        if ( items.length >= 16 && size < items.length * 0.25){
-            resize(items.length / 2);
-        } else if (size == items.length) {
-            resize(size * 2);
+    private void whetherResize() {
+        if (size() == items.length) {   // 8 -> 16 的情况
+            resize(items.length * 2);
         }
-
+        double proportion = (double) this.size / items.length;
+        if (this.items.length >= 16) {
+            if (proportion >= ExpandFactor) {
+                resize(items.length * 2);
+            } else if (proportion <= LessenFactor) {
+                resize(items.length / 2);
+            }
+        }
     }
 
-    private void resize(int capability){
+    // 测试方法
+    public int getItemsLength() {
+        return items.length;
+    }
+
+    private void resize(int capability) {
         T[] newItems = (T[]) new Object[capability];
-        int index = Math.floorMod(nextFirst + 1, items.length);
-        for (int i=0; i < size; i++) {
+        int index = floorMod(nextFirst+1, items.length);
+        for (int i=0; i < this.size; i++) {
             newItems[i] = items[index];
-            index = Math.floorMod(index + 1, items.length);
+            index = floorMod(index+1, items.length);
         }
         nextFirst = newItems.length - 1;
-        nextLast = size;
+        nextLast = this.size;
         items = newItems;
     }
 
+
+    private int floorMod(int v1, int v2) {
+        return Math.floorMod(v1, v2);
+    }
 
     @Override
     public void addFirst(T x) {
         whetherResize();
         items[nextFirst] = x;
+        nextFirst = floorMod(nextFirst - 1, items.length);
         size++;
-        nextFirst = Math.floorMod(nextFirst - 1, items.length);
 
     }
 
@@ -52,18 +69,18 @@ public class ArrayDeque61B<T> implements Deque61B<T>{
     public void addLast(T x) {
         whetherResize();
         items[nextLast] = x;
+        nextLast = floorMod(nextLast + 1, items.length);
         size++;
-        nextLast = Math.floorMod(nextLast + 1, items.length);
 
     }
 
     @Override
     public List<T> toList() {
-        List<T> L = new ArrayList<>();
-        for (int i = 0; i < this.size; i++){
-            L.add(this.items[i]);
+        List<T> returnList = new ArrayList<>();
+        for (int i=0; i<size; i++) {
+            returnList.add(get(i));
         }
-        return L;
+        return returnList;
     }
 
     @Override
@@ -78,21 +95,15 @@ public class ArrayDeque61B<T> implements Deque61B<T>{
 
     @Override
     public T removeFirst() {
-        if (isEmpty()){
+        if (isEmpty()) {
             return null;
         }
-
-        // nextFirst 是从右向左循环移动的，删除first的话, 需要让它向右循环移动一次
-        // 专门写一个 deletedIndex，只是为了代码更好读懂 TnT
-        int deletedIndex = Math.floorMod(nextFirst + 1, items.length);
-        T deletedItem = items[deletedIndex];
-        items[deletedIndex] = null;
-        nextFirst = Math.floorMod(nextFirst + 1, items.length);
+        nextFirst = floorMod(nextFirst+1, items.length);
+        T removedItem = items[nextFirst];
+        items[nextFirst] = null;
         size--;
-        // remove的是否resize应在size--之后判断
         whetherResize();
-
-        return deletedItem;
+        return removedItem;
     }
 
     @Override
@@ -100,47 +111,25 @@ public class ArrayDeque61B<T> implements Deque61B<T>{
         if (isEmpty()) {
             return null;
         }
-
-        // nextFirst 是从左向右循环移动的，删除last的话, 需要让它向左循环移动一次
-        int deletedIndex = Math.floorMod(nextLast - 1, items.length);
-        T deletedItem = items[deletedIndex];
-        items[deletedIndex] = null;
-        nextLast = Math.floorMod(nextLast - 1, items.length);
+        nextLast = floorMod(nextLast-1, items.length);
+        T removedItem = items[nextLast];
+        items[nextLast] = null;
         size--;
-        // remove的是否resize应在size--之后判断
         whetherResize();
-
-        return deletedItem;
+        return removedItem;
     }
 
     @Override
     public T get(int index) {
-        if (index < 0 || index >= size){
+        if (index < 0 || index >= size) {
             return null;
         }
-        // 举个例子，假设 index 为 0，则返回first的位置的数，即让nextFirst循环向右移动一次
-        // index 为其他数的话 则相应让 nextFirst + 1 向右移动 index 次
-        int returnIndex = Math.floorMod(nextFirst + 1 + index, items.length);
+        int returnIndex = floorMod(nextFirst+1+index, items.length);
         return items[returnIndex];
     }
 
     @Override
     public T getRecursive(int index) {
         throw new UnsupportedOperationException("No need to implement getRecursive for proj 1b");
-    }
-
-    public void printDeque(){
-        for (int i = Math.floorMod(nextFirst+1, items.length) ; i != nextLast; i = Math.floorMod(i+1, items.length)){
-            System.out.print(items[i] + " ");
-        }
-    }
-
-    public static void main(String[] args) {
-        ArrayDeque61B<Integer> l = new ArrayDeque61B<>();
-        l.addFirst(777);
-        l.addLast(999);
-        l.addFirst(4886);
-        l.addLast(984);
-        l.printDeque();
     }
 }
